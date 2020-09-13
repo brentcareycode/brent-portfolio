5af6ac7ae6652494519996f4ad28c179760bf351
Brent Carey <brentcarey@Brents-MacBook-Pro.local>
2020-09-12 20:16:36 -0400

    Created Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 function App() {
+  const [showForm, setShowForm] = useState(false)
+
+  const handleLinkClick = () => {
+    setShowForm(true)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        I would love to continue to learn React professionally. 
+      
         </p>
-        <a
+        {showForm ? 
+        <Form/> 
+        :
+          <p
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={handleLinkClick}
         >
-          Learn React
-        </a>
+         Hire Me
+        </p> }
+       
+       
       </header>
     </div>
   );
---
+++ src/Form.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Form.css'
+
+const Form = () => {
+  return (
+    <form className='Form'>
+    <h2>Enter your information below!</h2>
+    <label htmlFor='company'>Company Name:</label>
+    <br/>
+    <input placeholder='Hiring Company' id='company'/>
+    <br/>
+    <label htmlFor='recruiter'>Recruiter Name:</label>
+    <br/>
+    <input placeholder='Your Name' id='recruiter'/>
+    <br/>
+    <label htmlFor='email'>Contact Email:</label>
+    <br/>
+    <input type='email' placeholder='Your Email' id='email'/>
+    <br/>
+    <label htmlFor='phone'>Phone Number:</label>
+    <br/>
+    <input placeholder='Your Number' id='phone'/>
+    <br/>
+    <button className='submitButton'>Submit</button>
+    </form>
+  )
+}
+
+export default Form
