6505de27c5210cf4ba1043e69f09cf04787da356
Brent Carey <brentcarey@Brents-MacBook-Pro.local>
2020-09-13 00:34:03 -0400

    Added navbar logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import brentLaptop from './brentLaptop.png'
 import './NavBar.css'
 const NavBar = () => {
   return (
-    <div className='NavBar'>
-      <ul>
-        <li>Contact</li>
-        <li>Social</li>
-        <li>Hobbies</li>
-      </ul>
+    < div className='NavBar'>
+      <div className='nav-left'>
+        <img className='laptop-logo' alt='laptop-logo' src={brentLaptop} />
+      </div>
+      <div className='nav-right'>
+        <ul>
+          <li>Contact</li>
+          <li>Social</li>
+          <li>Hobbies</li>
+        </ul>
+      </div>
     </div>
   )
 }
